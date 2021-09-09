3c6f70ccca2be158eff0f25d7ce261bab261fb26
Ubuntu <ubuntu@ip-172-31-34-161.sa-east-1.compute.internal>
2021-09-09 14:27:46 +0000

    Gentellela OK

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -13,7 +13,17 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 # folder are already added.
 
 # /app/assets
-Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css )
+Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css 
+                                                  users_backoffice.js users_backoffice.css )
 
 # /lib/assets
-Rails.application.config.assets.precompile += %w( sb-admin-2.js sb-admin-2.css )
+Rails.application.config.assets.precompile += %w( sb-admin-2.js sb-admin-2.css
+                                                  custom.js custom.css googleapis.css
+                                                  img.jpg )
+# /vendor/assets
+Rails.application.config.assets.precompile += %w( jquery-2.2.3/dist/jquery
+                                                  bootstrap-4.3.1/dist/js/bootstrap.bundle.min
+                                                  bootstrap-4.3.1/dist/css/bootstrap
+                                                  font-awesome-4.6.3/css/font-awesome )
+
+
