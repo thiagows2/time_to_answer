6804738e519e9c42d0788099646a1acc13195c59
Thiago <thiagomarques408@gmail.com>
2021-09-10 15:45:24 +0000

    feature New Criada

--- app/controllers/admins_backoffice/admins_controller.rb
+++ app/controllers/admins_backoffice/admins_controller.rb
@@ -1,20 +1,50 @@
 class AdminsBackoffice::AdminsController < AdminsBackofficeController
+  before_action :verify_password, only: [:update]
+  before_action :set_admin, only: [:edit, :update]
+  
   def index
     @admins = Admin.all
   end  
   
+  def new
+    @admin = Admin.new
+  end
+  
+  def create
+    @admin = Admin.new(params_admin)
+    if @admin.save
+      redirect_to admins_backoffice_admins_path, notice: 'Adminstrador cadastrado com sucessso!'
+    else
+      render :new
+    end
+  end
+  
   def edit
-    @admin = Admin.find(params[:id])
   end
   
   def update
-    admin = Admin.find(params[:id])
-    params_admin = params.require(:admin).permit(:email, :password, :password_confirmation)
-    
-    if admin.update(params_admin)
+    params_admin 
+
+    if @admin.update(params_admin)
       redirect_to admins_backoffice_admins_path, notice: 'Adminstrador atualizado com sucessso!'
     else
       render :edit
     end
   end
+  
+  private
+  
+  def params_admin
+    params.require(:admin).permit(:email, :password, :password_confirmation)
+  end
+  
+  def verify_password
+    if params[:admin][:password].blank? && params[:admin][:password_confirmation].blank?
+      params[:admin].extract!(:password, :password_confirmation)
+    end
+  end
+  
+  def set_admin
+    @admin = Admin.find(params[:id])
+  end  
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,7 @@ Rails.application.routes.draw do
   end
   namespace :admins_backoffice do
     get 'welcome/index' #Deshboard
-    resources :admins, only: [:index, :edit, :update] #Admins
+    resources :admins, except: [:delete] #Admins
   end
   devise_for :admins
   devise_for :users
