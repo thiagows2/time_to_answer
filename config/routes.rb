299c26771aa1c84764335791f1c709c5a4a3f535
Thiago <thiagomarques408@gmail.com>
2021-09-10 19:16:56 +0000

    Subject controller create

--- config/routes.rb
+++ config/routes.rb
@@ -7,7 +7,8 @@ Rails.application.routes.draw do
   end
   namespace :admins_backoffice do
     get 'welcome/index' #Deshboard
-    resources :admins #Admins
+    resources :admins   #Admins
+    resources :subjects #Assuntos
   end
   devise_for :admins
   devise_for :users
