2e0eb6e3c238b34a94344d78625ea9a10ed58fdc
Thiago <thiagomarques408@gmail.com>
2021-09-13 15:15:44 +0000

    optimization problems, query n+1 and non-standard MVC

--- app/controllers/admins_backoffice/questions_controller.rb
+++ app/controllers/admins_backoffice/questions_controller.rb
@@ -1,8 +1,11 @@
 class AdminsBackoffice::QuestionsController < AdminsBackofficeController
   before_action :set_question, only: [:edit, :update, :destroy]
+  before_action :get_subjects, only: [:new, :edit]
 
   def index
-    @questions = Question.all.order(:description).page(params[:page])
+    @questions = Question.includes(:subject)
+                         .order(:description)
+                         .page(params[:page])
   end
 
   def new
@@ -45,5 +48,9 @@ class AdminsBackoffice::QuestionsController < AdminsBackofficeController
 
   def set_question
     @question = Question.find(params[:id])
+  end  
+  
+  def get_subjects
+    @subjects = Subject.all
   end
 end
