11c563ab775cfe9d40e57fea7318ed30e5424615
Thiago <thiagomarques408@gmail.com>
2021-09-15 13:32:38 +0000

    first adjustment in site layout

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -16,14 +16,14 @@ Rails.application.config.assets.paths << Rails.root.join('node_modules')
 Rails.application.config.assets.precompile += %w( admins_backoffice.js admins_backoffice.css 
                                                   users_backoffice.js users_backoffice.css
                                                   admin_devise.js admin_devise.css 
-                                                  user_devise.js user_devise.css )
+                                                  user_devise.js user_devise.css
+                                                  site.js site.css )
 
 # /lib/assets
 Rails.application.config.assets.precompile += %w( sb-admin-2.js sb-admin-2.css
                                                   custom.js custom.css googleapis.css
-                                                  img.jpg undraw_posting_photo.svg undraw_profile.svg
-                                                  undraw_profile_1.svg undraw_profile_2.svg
-                                                  undraw_profile_3.svg undraw_rocket.svg)
+                                                  img.jpg 
+                                                  navbar-top-fixed.css )
 # /vendor/assets
 Rails.application.config.assets.precompile += %w( jquery-2.2.3/dist/jquery
                                                   bootstrap-4.3.1/dist/js/bootstrap.bundle.min
