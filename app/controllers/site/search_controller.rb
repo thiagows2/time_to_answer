95321921a12a2faa48f67002d03642f7827f4e1c
Thiago <thiagomarques408@gmail.com>
2021-09-16 14:08:42 +0000

    search ON in welcome index

--- app/controllers/site/search_controller.rb
+++ app/controllers/site/search_controller.rb
@@ -1,6 +1,11 @@
 class Site::SearchController < SiteController
   def questions
-    @questions = Question.includes(:answers)
-                         .page(params[:page])
+    # @questions = Question.includes(:answers)
+    #                     .page(params[:page])
+                         
+     @questions = Question.includes(:answers)
+                          .where("lower(description) LIKE ?", "%#{params[:term].downcase}%")
+                          .page(params[:page])
+                          
   end
 end
