1f08e52d8c18012ddaa735c87af0d644535727a8
Thiago <thiagomarques408@gmail.com>
2021-09-16 15:02:09 +0000

    changing method to scopes in question model

--- app/models/question.rb
+++ app/models/question.rb
@@ -6,15 +6,16 @@ class Question < ApplicationRecord
   # Kaminari
   paginates_per 10
   
-  def self.search(page, term)
-    @questions = Question.includes(:answers)
-                         .where("lower(description) LIKE ?", "%#{term.downcase}%")
-                         .page(page)
-  end
+  # Scopes são usados em search's
+  scope :search, -> (page, term){
+    includes(:answers)
+    .where("lower(description) LIKE ?", "%#{term.downcase}%")
+    .page(page)
+  }
   
-  def self.last_questions(page)
-    @questions = Question.includes(:answers)
-                     .order('created_at desc')
-                     .page(page)
-  end
+  scope :last_questions, -> (page){
+    includes(:answers)
+    .order('created_at desc')
+    .page(page)
+  }
 end
