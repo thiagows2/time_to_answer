0ca38b5872d17a52ca3984f9d5ee8ac69543b46a
Thiago <thiagomarques408@gmail.com>
2021-09-20 12:50:04 +0000

    filtering questions by subject

--- app/models/question.rb
+++ app/models/question.rb
@@ -2,26 +2,24 @@ class Question < ApplicationRecord
   belongs_to :subject, inverse_of: :questions
   has_many :answers
   accepts_nested_attributes_for :answers, reject_if: :all_blank, allow_destroy: true
-  
+
   # Kaminari
-  paginates_per 10
-  
-  # Scopes são usados em search's
-  scope :_search_subject_, -> (page, term){
-  includes(:answers)
-  .where(subject_id: subject_id)
-  .page(page)
+  paginates_per 5
+
+  # Scopes
+  scope :_search_subject_, ->(page, subject_id){
+    includes(:answers, :subject)
+    .where(subject_id: subject_id)
+    .page(page)
   }
 
-  scope :search, -> (page, term){
-    includes(:answers)
+  scope :_search_, ->(page, term){
+    includes(:answers, :subject)
     .where("lower(description) LIKE ?", "%#{term.downcase}%")
     .page(page)
   }
-  
-  scope :last_questions, -> (page){
-    includes(:answers)
-    .order('created_at desc')
-    .page(page)
+
+  scope :last_questions, ->(page) {
+    includes(:answers, :subject).order('created_at desc').page(page)
   }
 end
